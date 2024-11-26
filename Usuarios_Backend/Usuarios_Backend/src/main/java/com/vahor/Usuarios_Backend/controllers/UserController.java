e184abbe0a6872a5a3a11485dc0fcf3570a56145
Gerardo Rubio Camacho <gerardo.rubio.camacho.ip@gmail.com>
2024-11-26 09:16:28 -0600

    Se anadió RequestMapping Faltante.

--- Usuarios_Backend/Usuarios_Backend/src/main/java/com/vahor/Usuarios_Backend/controllers/UserController.java
+++ Usuarios_Backend/Usuarios_Backend/src/main/java/com/vahor/Usuarios_Backend/controllers/UserController.java
@@ -14,9 +14,11 @@ import java.util.Optional;
 import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 
 @RestController
+@RequestMapping("/api/users")
 public class UserController {
     @Autowired
     private UserService service;
