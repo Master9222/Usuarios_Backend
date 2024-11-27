aef9b8cfbd44072873ecfa8334eab03ea5fd8adf
Gerardo Rubio Camacho <gerardo.rubio.camacho.ip@gmail.com>
2024-11-27 11:39:18 -0600

    anotación @Email para validar Email

--- Usuarios_Backend/Usuarios_Backend/src/main/java/com/vahor/Usuarios_Backend/entities/User.java
+++ Usuarios_Backend/Usuarios_Backend/src/main/java/com/vahor/Usuarios_Backend/entities/User.java
@@ -5,6 +5,7 @@ import jakarta.persistence.GeneratedValue;
 import static jakarta.persistence.GenerationType.*;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 
 @Entity
@@ -20,6 +21,7 @@ public class User {
     @NotEmpty
     private String lastname;
     @NotEmpty
+    @Email
     private String email;
     @NotEmpty
     private String username;
